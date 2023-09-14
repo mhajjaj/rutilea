698576c73ee6a562ebdaed3cd8decf1ae2215a6c
mhajjaj <mhajjaj7@gmail.com>
2023-09-14 10:39:30 +0900

    ...

---
+++ GearInspectionV6.py
@@ -0,0 +1,218 @@
+# Developed by m.hajjaj at rutilea
+import os
+
+from PIL import Image
+from tensorboardX import SummaryWriter
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+import requests
+import torch
+import torchvision
+import torchvision.transforms as transforms
+
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+from super_gradients.training import Trainer, dataloaders, models
+from super_gradients.training.dataloaders.dataloaders import (
+    coco_detection_yolo_format_train, coco_detection_yolo_format_val
+)
+from super_gradients.training.losses import PPYoloELoss
+from super_gradients.training.metrics import DetectionMetrics_050
+from super_gradients.training.models.detection_models.pp_yolo_e import (
+    PPYoloEPostPredictionCallback
+)
+
+class config:
+    #trainer params
+    HOME = os.getcwd()
+
+    CHECKPOINT_DIR = f'{HOME}\checkpoint\AGI-Dataset' #specify the path you want to save checkpoints to
+    EXPERIMENT_NAME = 'AGIExperiment' 
+
+    ##dataset params
+    DATA_DIR = f'{HOME}\GearInspection-Dataset' 
+    LOGS = f'{CHECKPOINT_DIR}\AGILogs'
+
+    TRAIN_IMAGES_DIR = 'train\images' 
+    TRAIN_LABELS_DIR = 'train\labels' 
+
+    VAL_IMAGES_DIR = 'valid\images'
+    VAL_LABELS_DIR = 'valid\labels' 
+
+    # if you have a test set
+    TEST_IMAGES_DIR = 'test\images' 
+    TEST_LABELS_DIR = 'test\labels' 
+
+    CLASSES = ['akkon', 'dakon', 'kizu', 'hakkon', 'kuromoyou', 'mizunokori', 'senkizu', 'yogore'] #what class names do you have
+
+    NUM_CLASSES = len(CLASSES)
+
+    DATALOADER_PARAMS={
+    'batch_size':8,
+    'num_workers':2
+    }
+
+    EPOCHS = 1
+    RUNNING_LOSS = 0.0
+    ACCURACY = 0.0
+
+    # model params
+    MODEL_NAME = 'yolo_nas_l' # choose from yolo_nas_s, yolo_nas_m, yolo_nas_l
+    PRETRAINED_WEIGHTS = 'coco' 
+
+# transforms
+transform = transforms.Compose(
+    [transforms.ToTensor(),
+    transforms.Normalize((0.5,), (0.5,))])
+
+# datasets
+trainset = torchvision.datasets.FashionMNIST(config.CHECKPOINT_DIR,
+    download=True,
+    train=True,
+    transform=transform)
+testset = torchvision.datasets.FashionMNIST(config.CHECKPOINT_DIR,
+    download=True,
+    train=False,
+    transform=transform)
+
+# dataloaders
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=config.DATALOADER_PARAMS['batch_size'],
+                                        shuffle=True, num_workers=config.DATALOADER_PARAMS['num_workers'])
+
+
+testloader = torch.utils.data.DataLoader(testset, batch_size=config.DATALOADER_PARAMS['batch_size'],
+                                        shuffle=False, num_workers=config.DATALOADER_PARAMS['num_workers'])
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(1, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 4 * 4, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 16 * 4 * 4)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+if __name__ == '__main__':
+    trainer = Trainer(experiment_name=config.EXPERIMENT_NAME, ckpt_root_dir=config.CHECKPOINT_DIR)
+
+    os.makedirs(config.LOGS, exist_ok=True)
+    writer = SummaryWriter(config.LOGS)
+
+    train_data = coco_detection_yolo_format_train(
+        dataset_params={
+            'data_dir': config.DATA_DIR,
+            'images_dir': config.TRAIN_IMAGES_DIR,
+            'labels_dir': config.TRAIN_LABELS_DIR,
+            'classes': config.CLASSES
+        },
+        dataloader_params=config.DATALOADER_PARAMS
+    )
+
+    val_data = coco_detection_yolo_format_val(
+        dataset_params={
+            'data_dir': config.DATA_DIR,
+            'images_dir': config.VAL_IMAGES_DIR,
+            'labels_dir': config.VAL_LABELS_DIR,
+            'classes': config.CLASSES
+        },
+        dataloader_params=config.DATALOADER_PARAMS
+    )
+
+    test_data = coco_detection_yolo_format_val(
+        dataset_params={
+            'data_dir': config.DATA_DIR,
+            'images_dir': config.TEST_IMAGES_DIR,
+            'labels_dir': config.TEST_LABELS_DIR,
+            'classes': config.CLASSES
+        },
+        dataloader_params=config.DATALOADER_PARAMS
+    )
+
+    train_data.dataset.plot()
+
+    model = models.get(config.MODEL_NAME,
+                       num_classes=config.NUM_CLASSES,
+                       pretrained_weights=config.PRETRAINED_WEIGHTS
+                       )
+
+    train_params = {
+        "average_best_models":True,
+        "warmup_mode": "linear_epoch_step",
+        "warmup_initial_lr": 1e-6,
+        "lr_warmup_epochs": 3,
+        "initial_lr": 5e-4,
+        "lr_mode": "cosine",
+        "cosine_final_lr_ratio": 0.1,
+        "optimizer": "Adam",
+        "optimizer_params": {"weight_decay": 0.0001},
+        "zero_weight_decay_on_bias_and_bn": True,
+        "ema": True,
+        "ema_params": {"decay": 0.9, "decay_type": "threshold"},
+        "max_epochs": config.EPOCHS, # Change No of epochs you want, more is better until a level
+        "mixed_precision": True,
+        "loss": PPYoloELoss(
+            use_static_assigner=False,
+            num_classes=config.NUM_CLASSES,
+            reg_max=16
+        ),
+        "valid_metrics_list": [
+            DetectionMetrics_050(
+                score_thres=0.1,
+                top_k_predictions=300,
+                num_cls=config.NUM_CLASSES,
+                normalize_targets=True,
+                post_prediction_callback=PPYoloEPostPredictionCallback(
+                    score_threshold=0.01,
+                    nms_top_k=1000,
+                    max_predictions=300,
+                    nms_threshold=0.7
+                )
+            )
+        ],
+        "metric_to_watch": 'mAP@0.50'
+    }
+
+    trainer.train(model=model,
+                training_params=train_params,
+                train_loader=train_data,
+                valid_loader=val_data)
+
+    best_model = models.get(config.MODEL_NAME,
+                        num_classes=config.NUM_CLASSES,
+                        checkpoint_path=os.path.join(config.CHECKPOINT_DIR, config.EXPERIMENT_NAME, 'average_model.pth'))
+
+    trainer.test(model=best_model,
+            test_loader=test_data, 
+            test_metrics_list=DetectionMetrics_050(score_thres=0.1,
+                                                   top_k_predictions=300,
+                                                   num_cls=config.NUM_CLASSES,
+                                                   normalize_targets=True,
+                                                   post_prediction_callback=PPYoloEPostPredictionCallback(score_threshold=0.01,
+                                                                                                          nms_top_k=1000,
+                                                                                                          max_predictions=300,
+                                                                                                          nms_threshold=0.7)
+                                                  ))
+    
+    writer.close()
+    PATH = f"{config.HOME}\AGIExperiment\AGIModel.pt"
+    torch.save(model.state_dict(), PATH)
+    
+    # For predication, please change conf.
+    img_path = 'GearInspection-Dataset/predict/year=2023-month=06-day=20-03_54_04-NG-2_0.png'
+    best_model.predict(img_path, conf=0.25).show()
+    best_model.predict(img_path, conf=0.50).show()
+    best_model.predict(img_path, conf=0.75).show()
