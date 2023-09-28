577853fa1fd539bf9b3025f78645544cce85d5e5
mhajjaj <mhajjaj7@gmail.com>
2023-09-28 19:19:12 +0900

    .

--- src/ocr.py
+++ src/ocr.py
@@ -1,6 +1,19 @@
 import os
 from predictor import Predictor as predictor
 
+import cv2 
+import os 
+from imutils import perspective
+import numpy as np 
+import json
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+from imutils import perspective
+
 HOME = os.getcwd()
 print(HOME)
 print("test")
+
+images_path = os.listdir(f"{HOME}\\..\\dataset\\train")
+
+print("Number of Training images are", len(images_path))
