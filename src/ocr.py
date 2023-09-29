ce6daaca9907ebae2c16570b298f40c3c60751d6
mhajjaj <mhajjaj7@gmail.com>
2023-09-29 19:33:05 +0900

    .

--- src/ocr.py
+++ src/ocr.py
@@ -98,27 +98,40 @@ custom_ocr = PaddleOCR(
     show_log=False
 )
 
-results = custom_ocr.ocr(img_path)
+custom_ocr = PaddleOCR(
+    use_angle_cls=True,
+    rec_model_dir='..\\model\\PaddleOCR\\inference\\en_PP-OCRv3_rec',
+    det_model_dir='..\\model\\PaddleOCR\\output\\det_db_inference',
+    # rec_char_dict_path='..\\model\\PaddleOCR\\ppocr\\utils\\dict90.txt',
+    lang='en',  
+    use_gpu=True,
+    show_log=True
+)
 
-img = cv2.imread(img_path)
-plt.imshow(img)
+# Iterate through the images in the folder
+for image_filename in os.listdir(images_path):
+    if image_filename.endswith((".jpg", ".jpeg", ".png")):
+        img_path = os.path.join(images_path, image_filename)
+        
+        # Perform OCR on the current image
+        results = custom_ocr.ocr(img_path)
 
-results = custom_ocr.ocr(img)
+        if not results:
+            print(f"No text found in {img_path}.")
+        else:
+            # Process the OCR results
+            print(f"Text found in {img_path}:")
+            for result in results[0]:
+                _, (text, confidence) = result
+                print(f'Text: {text}, Confidence: {confidence}')
 
-if not results:
-    print("No text found in the image.")
-else:
-    # Process the OCR results
-    for result in results[0]:
-        _, (text, confidence) = result
-        print(f'Text: {text}, Confidence: {confidence}')
+            # Display or save the result image with bounding boxes
+            image = Image.open(img_path).convert('RGB')
+            boxes = [detection[0] for line in results for detection in line]
+            txts = [detection[1][0] for line in results for detection in line]
+            scores = [detection[1][1] for line in results for detection in line]
 
-image = Image.open(img_path).convert('RGB')
-boxes = [detection[0] for line in results for detection in line]
-txts = [detection[1][0] for line in results for detection in line]
-scores = [detection[1][1] for line in results for detection in line]
-
-im_show = draw_ocr(image, boxes, txts, scores, font_path='simfang.ttf')
-im_show = Image.fromarray(im_show)
-im_show.show()
-im_show.save('result_ocr.jpg')
+            im_show = draw_ocr(image, boxes, txts, scores, font_path='simfang.ttf')
+            im_show = Image.fromarray(im_show)
+            # im_show.show()
+            im_show.save(f'..\\dataset\\output\\{image_filename}')
